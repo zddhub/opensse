1e5d9a1adcf2c76f3a29c5a19a881544024c982c
zdd <zddhub@gmail.com>
2017-02-14 00:23:09 +0800

    add opensse.h

---
+++ opensse.h
@@ -0,0 +1,12 @@
+#ifndef OPENSSE_H
+#define OPENSSE_H
+
+#include "opensse/common/distance.h"
+#include "opensse/common/types.h"
+#include "opensse/features/galif.h"
+#include "opensse/index/invertedindex.h"
+#include "opensse/io/filelist.h"
+#include "opensse/io/reader_writer.h"
+#include "opensse/quantize/quantizer.h"
+
+#endif
