757f38c8c5be518e67ab1ac942b7be1a22725f8a
zdd <zddhub@gmail.com>
2017-02-13 18:55:06 +0800

    use cmake to compile opensse

--- common/types.h
+++ common/types.h
@@ -26,6 +26,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs/imgcodecs.hpp>
 
 #include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
