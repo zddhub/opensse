b22f2c872e9e5b1c2a1bb3f8b72261cb9deccf78
zdd <zddhub@gmail.com>
2017-04-11 23:29:46 +0800

    use producer-consumer in c++

--- tools/extract.cpp
+++ tools/extract.cpp
@@ -16,6 +16,7 @@
 **************************************************************************/
 #include <iostream>
 #include <fstream>
+#include <queue>
 
 using namespace std;
 
@@ -31,6 +32,54 @@ void usages() {
          << "  -o\t \033[4moutput\033[0m" <<endl;
 }
 
+std::mutex m;
+std::condition_variable cond;
+std::queue<Features_t> FeaturesQueue;
+bool finished = false;
+
+void extracter(const FileList &files) {
+
+    Galif *galif = new Galif();
+
+    for(uint i = 0; i < files.size(); i++) {
+        {
+            std::lock_guard<std::mutex> lock(m);
+
+            KeyPoints_t keypoints;
+            Features_t features;
+            cv::Mat image = cv::imread(files.getFilename(i));
+
+            galif->compute(image, keypoints, features);
+            FeaturesQueue.push(features);
+        }
+
+        cond.notify_all();
+    }
+    {
+        std::lock_guard<std::mutex> lock(m);
+        finished = true;
+    }
+    cond.notify_all();
+}
+
+void writer(const std::string &filename, uint sizes) {
+    ofstream ft_out(filename.c_str());
+    ft_out << sizes << endl;
+    uint i = 0;
+    while (true) {
+        std::unique_lock<std::mutex> lock(m);
+        cond.wait(lock, []{ return finished || !FeaturesQueue.empty(); });
+        while (!FeaturesQueue.empty()) {
+            write(FeaturesQueue.front(), ft_out);
+            cout << "Extract descriptors " << i++ << "/" << sizes << "\r" << flush;
+            FeaturesQueue.pop();
+        }
+        if (finished) break;
+    }
+    cout << "Extract descriptors "<< i << "/" << sizes <<  "." <<endl;
+    ft_out.close();
+}
+
 int main(int argc, char *argv[])
 {
     if(argc != 5) {
@@ -41,32 +90,12 @@ int main(int argc, char *argv[])
     FileList files;
     files.load(argv[2]);
 
-    Galif *galif = new Galif();
-
-    std::vector<KeyPoints_t> vecKeypoints;
-    std::vector<Features_t> vecFeatures;
-
-    // Don't keep keypoints save memory.
-    // std::string kp_file = std::string(argv[4]) + "keypoints";
-    // ofstream kp_out(kp_file.c_str());
     std::string ft_file = std::string(argv[4]);
-    ofstream ft_out(ft_file.c_str());
 
-    // kp_out << files.size() << endl;
-    ft_out << files.size() << endl;
-    for(uint i = 0; i < files.size(); i++) {
-        KeyPoints_t keypoints;
-        Features_t features;
-        cv::Mat image = cv::imread(files.getFilename(i));
-        galif->compute(image, keypoints, features);
-        // write(keypoints, kp_out);
-        write(features, ft_out);
-        cout << "Extract descriptors " << i+1 << "/" << files.size() <<"\r" << flush;
-    }
-    cout << "Extract descriptors "<< files.size() << "/" << files.size() <<  "." <<endl;
-
-    // kp_out.close();
-    ft_out.close();
+    std::thread t1(extracter, files);
+    std::thread t2(writer, ft_file, files.size());
+    t1.join();
+    t2.join();
 
     return 0;
 }
